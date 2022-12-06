87aefb0c7ff51083d53bf0f562cefe3f88bdff53
Jayro Alhuay <jalhuayg@emeal.nttdata.com>
2022-12-05 19:04:54 -0500

    se agrego un contador

--- ProyectoFinal/Product/DetailViewController.swift
+++ ProyectoFinal/Product/DetailViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 class DetailViewController: UIViewController {
 
     var detailProducts : Products?
+
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var precioLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imgImageView: UIImageView!
+    
+    
+    // Contador
+    
+    var count = 1
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBAction func downButton(_ sender: UIButton) {
+        if(count <= 1) {return}
+        count = count - 1
+        countLabel.text = "\(count)"
+    }
+    @IBAction func upButton(_ sender: UIButton) {
+        
+        count = count + 1
+        countLabel.text = "\(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +41,7 @@ class DetailViewController: UIViewController {
         
     }
     
-
+    
+    
 
 }
